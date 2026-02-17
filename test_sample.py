2e4606be0a81164af0fa6e7156340603765fda50
Akanksha-QA-dot <madakeakanksha230@gmail.com>
2026-02-17 15:59:09 +0530

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,10 +1,1 @@
-
-def test_addition():
-    assert 2 + 2 == 4
-
-def test_string():
-    assert "qa" in "qa engineer"
-
-def test_list():
-    items = ["login", "checkout", "search"]
-    assert len(items) == 3
+assert 2 + 2 == 4
